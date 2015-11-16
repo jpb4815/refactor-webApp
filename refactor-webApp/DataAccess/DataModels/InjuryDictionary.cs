94cb226591ef62ab3b1089206e81584caa8f30af
John Brown <john.brown01@mymail.champlain.edu>
2015-11-16 06:27:12 -0500

    pushing unversioned files

---
+++ refactor-webApp/DataAccess/DataModels/InjuryDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations.Model;
+using System.Linq;
+using System.Web;
+
+namespace DataAccess.DataModels
+{
+    public class InjuryDictionary
+    {        
+        public int Id { get; set; }
+        public string Injury { get; set; }
+        public string Description { get; set; }
+        public string IcdCode { get; set; }       
+    }
+}
